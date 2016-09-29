a8502cd4c89541e919317bea56b394d4f6202d8b
Pwl Webdev <pwl.webdev@gmail.com>
2016-09-29 19:50:45 +0200

    Slider with buttons working

--- assets/scripts/carousel.js
+++ assets/scripts/carousel.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
 	console.log("ready");
+
+	$( "#button_left" ).click(function() {
+  		previous();
+	});
+	$( "#button_right" ).click(function() {
+  		next();
+	});
 });
+
+var next = function(){
+	var offset = $('.frame').css('left');
+	offset = parseInt(offset, 10);
+	if(offset > -2400){
+		offset -= 600;
+	} else{
+		offset = 0;
+	}
+	console.log("offset"+offset);
+	$('.frame').css('left',offset+"px");
+}
+var previous = function(){
+	var offset = $('.frame').css('left');
+	offset = parseInt(offset, 10);
+	if(offset < 0){
+		offset += 600;
+	} else{
+		offset = -2400;
+	}
+	console.log("offset"+offset);
+	$('.frame').css('left',offset+"px");
+}
